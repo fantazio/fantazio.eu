06d4100e9c689969904751991805cf67e2012bb9
Corentin De Souza <9597216+fantazio@users.noreply.github.com>
2026-04-17 17:32:04 +0200

    yocaml: follow article indexation
    https://yocaml.github.io/tutorial/simple-blog-index.html

--- bin/blog.ml
+++ bin/blog.ml
@@ -17,6 +17,8 @@ let track_binary =
 let with_ext exts file =
   List.exists (fun ext -> Path.has_extension ext file) exts
 
+let is_markdown file = with_ext [ "md"; "markdown"; "mdown" ] file
+
 let build_paths dir filenames =
   List.map (fun filename -> Path.(dir / filename)) filenames
 
@@ -41,6 +43,10 @@ let document_archetype : document_kind -> (module ARCHETYPE) = function
   | Page -> (module Archetype.Page)
   | Article -> (module Archetype.Article)
 
+let compute_link source =
+  let into = Path.abs [ "articles" ] in
+  source |> Path.move ~into |> Path.change_extension "html"
+
 let create_document document_kind source =
   let www_target = www_target document_kind source in
   let module Archetype = (val document_archetype document_kind) in
@@ -62,12 +68,37 @@ let create_page source = create_document Page source
 let create_article source = create_document Article source
 
 let create_documents document_kind =
-  let is_markdown file = with_ext [ "md"; "markdown"; "mdown" ] file in
   let paths = match document_kind with Page -> pages | Article -> articles in
   Batch.iter_files ~where:is_markdown paths (create_document document_kind)
 
 let create_pages = create_documents Page
 let create_articles = create_documents Article
+
+let fetch_articles =
+  Archetype.Articles.fetch ~where:is_markdown ~compute_link
+    (module Yocaml_yaml)
+    articles
+
+let create_index =
+  let source = Path.(content / "index.md") in
+  let www_index = www_target Page source in
+  let pipeline =
+    let open Task in
+    let templates = Path.(templates / "index.html") :: get_templates Page in
+    let+ () = track_binary
+    and+ apply_templates = Yocaml_jingoo.read_templates templates
+    and+ articles = fetch_articles
+    and+ metadata, content =
+      Yocaml_yaml.Pipeline.read_file_with_metadata
+        (module Archetype.Page)
+        source
+    in
+    let metadata = Archetype.Articles.with_page ~page:metadata ~articles in
+    content
+    |> Yocaml_markdown.from_string_to_html
+    |> apply_templates (module Archetype.Articles) ~metadata
+  in
+  Action.Static.write_file www_index pipeline
 
 let create_css =
   let www_style = Path.(www / "style.css") in
@@ -94,6 +125,7 @@ let program () =
   >>= create_css
   >>= create_pages
   >>= create_articles
+  >>= create_index
   >>= Action.store_cache cache
 
 let () =
---
+++ content/articles/my_second_article.md
@@ -0,0 +1,7 @@
+---
+title: My second Article
+description: Here is my econdirst article
+date: 2026-09-08
+---
+
+Hello **World**! This is my first article
